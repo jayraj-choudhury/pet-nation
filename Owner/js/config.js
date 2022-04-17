643c5b87a8e6f5d4fca8c3f633c33da962163818
jayraj-choudhury <jayraj.choudhury@quantiphi.com>
2022-04-17 19:10:13 +0530

    change in user pools

--- Owner/js/config.js
+++ Owner/js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_sxcpkqa2i', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-east-1_pj892HAyF', // e.g. us-east-2_uXboG5pAb
         region: 'us-east-1', // e.g. us-east-2
-		clientId: '42kvrkbgur8ehoeqfjaddg8gh' //is this used anywhere?
+		clientId: '4pahb08pl8q1442amip2rbh341' //is this used anywhere?
     },
 };
 
--- Volunteer/js/config.js
+++ Volunteer/js/config.js
@@ -1,8 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_r5GZXIhvB', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-east-1_p2uggH5GT', // e.g. us-east-2_uXboG5pAb
         region: 'us-east-1', // e.g. us-east-2
-		clientId: '45j3e6er8nc239pdeiqukt6cca' //is this used anywhere?
+		clientId: '7c6o647t9po55eh13434fpnsl1' //is this used anywhere?
     },
 };
-
